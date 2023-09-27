e649d7ba1b02b42fd73e0c05f68d68b956e3a5d1
lrshsl <hoeslilars@gmail.com>
2023-09-27 19:39:54 +0200

    Shape recognision is acceptable

--- pytorch/fresh_rtov.py
+++ pytorch/fresh_rtov.py
@@ -19,10 +19,10 @@ transform = transforms.Compose([
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
-batch_size = 5
-epochs = 4
+batch_size = 4
+epochs = 10
 
-trainset = LazyDataset(img_dim=Vec3(32, 32, 3), num_samples=1000, transform=transform)
+trainset = LazyDataset(img_dim=Vec3(32, 32, 3), num_samples=2000, transform=transform)
 trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
 
 testset = LazyDataset(img_dim=Vec3(32, 32, 3), num_samples=100, transform=transform)
@@ -38,21 +38,36 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 6, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(6, 16, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+        self.shape_fc = nn.Sequential(
+            nn.Linear(16 * 5 * 5, 120),
+            nn.Linear(120, 84),
+            nn.Linear(84, 10),
+            nn.ReLU(),
+        )
+        self.p1_fc = nn.Sequential(
+            nn.Linear(16 * 5 * 5, 120),
+            nn.Linear(120, 84),
+            nn.Linear(84, 10),
+            nn.ReLU(),
+        )
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        x = self.conv1(x)
+        x = self.conv2(x)
         x = torch.flatten(x, 1) # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        shape = self.shape_fc(x)
+        # p1 = self.p1_fc(x)
+        return shape
 
 
 net = Net()
@@ -67,12 +82,12 @@ optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 # }}}
 
 # Training {{{
+losses = []
 for epoch in range(epochs):  # loop over the dataset multiple times
 
     running_loss = 0.0
-    for i, data in enumerate(trainloader):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
+    images_per_epoch = 0
+    for i, (inputs, labels) in enumerate(trainloader):
 
         # zero the parameter gradients
         optimizer.zero_grad()
@@ -85,15 +100,22 @@ for epoch in range(epochs):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-            running_loss = 0.0
+        images_per_epoch += 1
 
+    print(f'[{epoch + 1}, {epoch + 1:5d}] loss: {running_loss / 2000:.3f}')
+    losses.append(running_loss / images_per_epoch)
+    running_loss = 0.0
+
+
+plt.plot(losses)
+plt.show()
 print('Finished Training')
 # }}}
 
 # Testing {{{
 images, labels = next(iter(testloader))
+images_list = list(images)
+labels_list = list(labels)
 preds = net(images)
 
 cols = 4
@@ -103,12 +125,31 @@ fig, axis = plt.subplots(rows, cols, figsize=(10, 10))
 
 for i in range(len(labels)):
     ax = axis[i // cols, i % cols]
-    ax.imshow(images[i].permute(1, 2, 0))
+    image = images[i].permute(1, 2, 0) * 255
+    ax.imshow(image.cpu().numpy())
     ax.set_title(shape_names[preds[i].argmax().item()])
     ax.axis('off')
 
 plt.tight_layout()
 plt.show()
+print('Finished testing')
 
+# test_accuracy = (preds.argmax(dim=1) == labels).sum().item()
+
+# if input(f'Performance: {test_accuracy}\nSave model? (y/N)') == 'y':
+#     name = input('Name: ') + '.pt'
+#     torch.save(net.state_dict(), 'model.pt')
+
+# if input('Continue training? (y/N)') == 'y':
+    # epochs = input('How many epochs?')
+    # train(net, criterion, optimizer, epochs)
+
+# net.load_state_dict(torch.load('model.pt'))
 # }}}
+
+
+
+
+
+
 
